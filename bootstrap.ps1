7c0c3a092e134aa91222e0ef5bddfa2f51415bad
Kyle Benson <kyle@psadmin.io>
2020-03-05 10:30:08 -0600

    updated bootstrap example

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,8 +1,11 @@
-# init
-Set-ExecutionPolicy RemoteSigned -force
-cd "c:/temp"
-(Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')))
-choco install git -y
-git clone https://github.com/psadmin-io/psft-pi-baker.git
-# install
-./psft-pi-baker/win2016.ps1
+
+  # init
+  Set-ExecutionPolicy RemoteSigned -force
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls13, [Net.SecurityProtocolType]::Tls12 
+  cd "c:/temp"
+  (Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')))
+  choco install git -y
+  git clone https://github.com/psadmin-io/psft-pi-baker.git
+  # install
+  cd "./psft-pi-baker"
+  ./win2016.ps1 -MOS_USERNAME "${mos_username}" -MOS_PASSWORD "${mos_password}" -MOS_PATCH_ID "${mos_patch_id}"
