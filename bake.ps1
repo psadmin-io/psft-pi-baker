5487fc62f959a3a1ce4f8389b15d71991af7b0e2
Kyle Benson <kyle@psadmin.io>
2020-03-05 16:16:45 -0600

    more bake cleanup

--- bake.ps1
+++ bake.ps1
@@ -10,64 +10,74 @@
     .EXAMPLE
         provision-utilities.ps1 
 #>
-#-----------------------------------------------------------[Parameters]----------------------------------------------------------
-[CmdletBinding()]
+
+[CmdletBinding(DefaultParameterSetName="Help")]
+
 Param(
+#    [Parameter (Mandatory=$true, ParameterSetName="Install")][Alias("i")][switch] $Install,
+#    [Parameter (Mandatory=$false, ParameterSetName="Help")] [switch] $Help
     [String]$MOS_USERNAME = $env:MOS_USERNAME,
-    [String]$MOS_PASSWORD = $env:MOS_PASSWORD,
+    [String]$MOS_PASSWORD = $env:MOS_PASSWORD, # TODO secure string
     [String]$MOS_PATCH_ID = $env:MOS_PATCH_ID
 )
-#---------------------------------------------------------[Initialization]--------------------------------------------------------
-# Valid values: "Stop", "Inquire", "Continue", "Suspend", "SilentlyContinue"
-$ErrorActionPreference = "Stop"
-$DebugPreference = "SilentlyContinue"
-$VerbosePreference = "SilentlyContinue"
 
-#-----------------------------------------------------------[Variables]-----------------------------------------------------------
-$log = "c:\temp\psft-pi-baker.log"
+Begin {
+    # TODO - params?
+    # Valid values: "Stop", "Inquire", "Continue", "Suspend", "SilentlyContinue"
+    $ErrorActionPreference = "Stop"
+    $DebugPreference = "SilentlyContinue"
+    $VerbosePreference = "SilentlyContinue"
 
-#-----------------------------------------------------------[Functions]-----------------------------------------------------------
-Function log($msg) {
-    $stamp = [DateTime]::Now.ToString("yyyyMMdd-HHmmss")
-    Add-Content $log "[$stamp] $msg"
+    $log = "c:\temp\psft-pi-baker.log" # TODO
+
+    Push-Location $PSScriptRoot
+
+    Function log($msg) {
+        $stamp = [DateTime]::Now.ToString("yyyyMMdd-HHmmss")
+        Add-Content $log "[$stamp] $msg"
+    }
+    Function info($msg) {
+        log("INFO: $msg")
+    }
+    Function debug($msg) {
+        log("DEBUG: $msg")
+    }
+    Function error($msg) {
+        log("ERROR: $msg")
+    }    
 }
-Function info($msg) {
-    log("INFO: $msg")
+
+Process {
+    # Setup file structure
+    Try {        
+        info("Setting up file structure.")
+        New-Item -ItemType directory -Path "c:/psft/dpk/downloads" -Force
+        New-Item -ItemType directory -Path "c:/vagrant/config" -Force
+        New-Item -ItemType directory -Path "c:/vagrant/scripts" -Force
+        Copy-Item -Path "./files/psft_customizations-win.yaml" -Destination "c:/vagrant/config/psft_customizations.yaml" 
+        Copy-Item -Path "./files/vagabond.json" -Destination "c:/vagrant/scripts/vagabond.json" 
+    } Catch {
+        error($_.Exception.Message)
+        Break
+    }
+
+    # Run provisoners
+    info("banner")
+    & ./powershell/banner.ps1 >> $log
+    info("download")
+    & ./powershell/provision-download.ps1 -MOS_USERNAME "$MOS_USERNAME" -MOS_PASSWORD "$MOS_PASSWORD" -PATCH_ID "$MOS_PATCH_ID" -DPK_INSTALL "c:/psft/dpk/downloads/$MOS_PATCH_ID" >> $log
+    info("bootstrap")
+    & ./powershell/provision-bootstrap-ps.ps1 -PATCH_ID "$MOS_PATCH_ID" -DPK_INSTALL "c:/psft/dpk/downloads/$MOS_PATCH_ID" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet" >> $log
+    info("yaml")
+    & ./powershell/provision-yaml.ps1 -DPK_INSTALL "c:/psft/dpk/downloads/$MOS_PATCH_ID" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet" >> $log
+    info("puppet apply")
+    & ./powershell/provision-puppet-apply.ps1 -DPK_INSTALL "c:/psft/dpk/downloads/$MOS_PATCH_ID" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet" >> $log
+    info("util")
+    & ./powershell/provision-puppet-utilities.ps1 >> $log
+
+    info("done done.")
 }
-Function debug($msg) {
-    log("DEBUG: $msg")
+
+End {
+    Pop-Location
 }
-Function error($msg) {
-    log("ERROR: $msg")
-}
-
-#-----------------------------------------------------------[Execution]-----------------------------------------------------------
-# testing log
-info("Testing info")
-debug("Testing debug")
-error("Testing error")
-
-# Setup file structure
-info("Setting up file structure...")
-New-Item -ItemType directory -Path "c:/psft/dpk/downloads"
-New-Item -ItemType directory -Path "c:/vagrant/config"
-New-Item -ItemType directory -Path "c:/vagrant/scripts"
-Copy-Item -Path "./files/psft_customizations-win.yaml" -Destination "c:/vagrant/config/psft_customizations.yaml" 
-Copy-Item -Path "./files/vagabond.json" -Destination "c:/vagrant/scripts/vagabond.json" 
-info("...done.")
-
-# Run provisoners
-info("banner")
-& ./powershell/banner.ps1 > $log
-info("downnload")
-& ./powershell/provision-download.ps1 -MOS_USERNAME "$MOS_USERNAME" -MOS_PASSWORD "$MOS_PASSWORD" -PATCH_ID "$MOS_PATCH_ID" -DPK_INSTALL "c:/psft/dpk/downloads/$MOS_PATCH_ID" > $log
-info("bootstrap")
-& ./powershell/provision-bootstrap-ps.ps1 -PATCH_ID "${MOS_PATCH_ID}" -DPK_INSTALL "c:/psft/dpk/downloads/${MOS_PATCH_ID}" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet" > $log
-info("yaml")
-& ./powershell/provision-yaml.ps1 -DPK_INSTALL "c:/psft/dpk/downloads/${MOS_PATCH_ID}" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet" > $log
-info("puppet apply")
-& ./powershell/provision-puppet-apply.ps1 -DPK_INSTALL "c:/psft/dpk/downloads/${MOS_PATCH_ID}" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet" > $log
-info("util")
-& ./powershell/provision-puppet-utilities.ps1 > $log
-
-info("done done.")
--- examples/bootstrap.ps1
+++ examples/bootstrap.ps1
@@ -5,8 +5,9 @@
   New-Item -ItemType directory -Path "c:/temp" #todo pass location?
   Push-Location "c:/temp"
   (Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')))
+  refreshenv
   choco install git -y
-  powershell -Command "git clone https://github.com/psadmin-io/psft-pi-baker.git"
+  refreshenv # todo refresh is not working?
+  C:\PROGRA~1\Git\bin\git clone https://github.com/psadmin-io/psft-pi-baker.git
   # install
-  Push-Location "./psft-pi-baker"
-  ./bake.ps1 -MOS_USERNAME "${mos_username}" -MOS_PASSWORD "${mos_password}" -MOS_PATCH_ID "${mos_patch_id}"
+  ./psft-pi-baker/bake.ps1 -MOS_USERNAME "${mos_username}" -MOS_PASSWORD "${mos_password}" -MOS_PATCH_ID "${mos_patch_id}"
