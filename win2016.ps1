fb2c7591a4d1115b416dcdc6f6ec2cefc7b322cd
Kyle Benson <kyle@psadmin.io>
2020-03-05 11:33:53 -0600

    added basic logging, needs work

--- win2016.ps1
+++ win2016.ps1
@@ -26,18 +26,47 @@ $VerbosePreference = "SilentlyContinue"
 #-----------------------------------------------------------[Variables]-----------------------------------------------------------
 
 #-----------------------------------------------------------[Functions]-----------------------------------------------------------
+Function log($msg) {
+    $stamp = [DateTime]::Now.ToString("yyyyMMdd-HHmmss")
+    Add-Content c:\temp\psft-pi-baker.log "[$stamp] $msg"
+}
+Function info($msg) {
+    log("INFO: $msg")
+}
+Function debug($msg) {
+    log("DEBUG: $msg")
+}
+Function error($msg) {
+    log("ERROR: $msg")
+}
 
 #-----------------------------------------------------------[Execution]-----------------------------------------------------------
+# testing log
+info("Testing info")
+debug("Testing debug")
+error("Testing error")
 
+# Setup file structure
+info("Setting up file structure...")
 New-Item -ItemType directory -Path "c:/psft/dpk/downloads"
 New-Item -ItemType directory -Path "c:/vagrant/config"
 New-Item -ItemType directory -Path "c:/vagrant/scripts"
 Copy-Item -Path "./config/psft_customizations-win.yaml" -Destination "c:/vagrant/config/psft_customizations.yaml" 
 Copy-Item -Path "./config/vagabond.json" -Destination "c:/vagrant/scripts/vagabond.json" 
+info("...done.")
 
+# Run provisoners
+info("banner")
 & ./powershell/banner.ps1
+info("downnload")
 & ./powershell/provision-download.ps1 -MOS_USERNAME "$MOS_USERNAME" -MOS_PASSWORD "$MOS_PASSWORD" -PATCH_ID "$MOS_PATCH_ID" -DPK_INSTALL "c:/psft/dpk/downloads/$MOS_PATCH_ID"
+info("bootstrap")
 & ./powershell/provision-bootstrap-ps.ps1 -PATCH_ID "${MOS_PATCH_ID}" -DPK_INSTALL "c:/psft/dpk/downloads/${MOS_PATCH_ID}" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet"
+info("yaml")
 & ./powershell/provision-yaml.ps1 -DPK_INSTALL "c:/psft/dpk/downloads/${MOS_PATCH_ID}" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet"
+info("puppet apply")
 & ./powershell/provision-puppet-apply.ps1 -DPK_INSTALL "c:/psft/dpk/downloads/${MOS_PATCH_ID}" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet"
+info("util")
 & ./powershell/provision-puppet-utilities.ps1
+
+info("done done.")
