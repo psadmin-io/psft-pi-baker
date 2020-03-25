c6d6f22251545e5029393fb22a7cadbc191c3067
Dan Iverson <dan@daniverson.com>
2020-03-24 22:48:59 -0500

    Add 8.58 and prebuild cache

--- powershell/provision-puppet-apply.ps1
+++ powershell/provision-puppet-apply.ps1
@@ -127,14 +127,46 @@ function execute_puppet_apply() {
   } # end switch
 }
 
+function prebuild_cache() {
+  Write-Output "Prebuilding Application Cache"
+  # Reset Environment and PATH to include bin\puppet
+  $env:PATH = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+  switch ($TOOLS_MINOR_VERSION) {
+    "55" {
+      write-output "8.55 DPK not supported for preloading cache"
+    }
+    "56" {
+      execute_prebuild_cache
+    }
+    "57" {
+      execute_prebuild_cache
+    }
+    "58" {
+      execute_prebuild_cache
+    }
+  } # end switch
+}
+
+function execute_prebuild_cache() {
+  if ($DEBUG -eq "true") {
+    puppet module install puppetlabs-powershell --confdir ${PUPPET_HOME}
+    invoke-webrequest -method GET -uri https://gist.githubusercontent.com/iversond/60f0e820bc476f16d5d6b2161fbbafbb/raw/fixdpkbug.pp -outfile "c:\temp\fixdpkbug.pp"
+    invoke-webrequest -method GET -uri https://gist.githubusercontent.com/iversond/48af2c095c883277fe08f85d415739b1/raw/loadcache.pp -outfile "c:\temp\loadcache.pp"
+    puppet apply c:\temp\fixdpkbug.pp --confdir="${PUPPET_HOME}"
+    puppet apply c:\temp\loadcache.pp --confdir="${PUPPET_HOME}"
+  } else {
+    puppet module install puppetlabs-powershell --confdir ${PUPPET_HOME} 2>&1 | out-null
+    invoke-webrequest -method GET -uri https://gist.githubusercontent.com/iversond/60f0e820bc476f16d5d6b2161fbbafbb/raw/fixdpkbug.pp -outfile "c:\temp\fixdpkbug.pp" 2>&1 | out-null
+    invoke-webrequest -method GET -uri https://gist.githubusercontent.com/iversond/48af2c095c883277fe08f85d415739b1/raw/loadcache.pp -outfile "c:\temp\loadcache.pp" 2>&1 | out-null
+    puppet apply c:\temp\fixdpkbug.pp --confdir="${PUPPET_HOME}" 2>&1 | out-null
+    puppet apply c:\temp\loadcache.pp --confdir="${PUPPET_HOME}" 2>&1 | out-null
+  }
+}
+
 #-----------------------------------------------------------[Execution]-----------------------------------------------------------
 
 . determine_tools_version
 . determine_puppet_home
 . execute_puppet_apply
-
-# $fqdn = facter fqdn
-# $port = hiera pia_http_port
-# $sitename = hiera pia_site_name
-
-# Write-Output "Your login URL is http://${fqdn}:${port}/${sitename}/signon.html" -ForegroundColor White
+. prebuild_cache
