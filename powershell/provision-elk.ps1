68a03e02115308d2aab83c9392d770e2ed243fc7
Dan Iverson <dan@psadmin.io>
2020-04-25 16:51:50 -0500

    silentinstall.config file populated with encrypted passwords

--- powershell/provision-elk.ps1
+++ powershell/provision-elk.ps1
@@ -14,20 +14,19 @@
     .DESCRIPTION
         ELK provisioning bootstrap script for ps-vagabond
 
-    .PARAMETER PATCH_ID
-        Patch ID for the ELK DPK
-
-    .PARAMETER MOS_USERNAME
-        My Oracle Support Username
-
-    .PARAMETER MOS_PASSWORD
-        My Oracle Support Password
-
     .PARAMETER ELK_INSTALL
-        Directory to use for downloading the ELK files
+        Directory to use for downloading the ELK file
+    .PARAMETER APP
+        Application Type: FSCM92, HCM92, ELM92, IH92, CS92
+    .PARAMETER PUPPET_HOME
+        Puppet Home for the DPK
+    .PARAMETER ESADMIN_PWD
+        Password to encrypt for esadmin
+    .PARAMETER PEOPLE_PWD
+        Password to encrypt for people
 
     .EXAMPLE
-        provision-elk.ps1 -PATCH_ID 23711856 -MOS_USERNAME user@example.com -MOS_PASSWORD mymospassword -ELK_INSTALL C:/peoplesoft/dpk/elk7_01
+        provision-elk.ps1 -ELK_INSTALL C:/peoplesoft/dpk/download/elk7
 
 #>
 
@@ -35,8 +34,11 @@
 
 [CmdletBinding()]
 Param(
-  [String]$ELK_INSTALL  = $env:ELK_INSTALL
-  [String]$APP          = $env:APP
+  [String]$ELK_INSTALL  = $env:ELK_INSTALL,
+  [String]$APP          = $env:APP,
+  [String]$ESADMIN_PWD  = $env:ESADMIN_PWD,
+  [String]$PEOPLE_PWD   = $env:PEOPLE_PWD,
+  [String]$PUPPET_HOME  = $env:PUPPET_HOME
 )
 
 
@@ -54,7 +56,7 @@ If ( ${ELK_INSTALL} -eq '' ) { Write-Output "ELK_INSTALL must be passed" }
 $DEBUG = "false"
 
 function determine_elk_version() {
-    $ELK_VERSION = $(Get-Content ${ELK_INSTALL}}/elasticsearch-manifest | select-string "version" | % {$_.line.split("=")[1]})
+    $ELK_VERSION = $(Get-Content ${ELK_INSTALL}/elasticsearch-manifest | select-string "^version" | % {$_.line.split("=")[1]})
     $ELK_MAJOR_VERSION = $ELK_VERSION.split(".")[0]
     $ELK_MINOR_VERSION = $ELK_VERSION.split(".")[1]
     $ELK_PATCH_VERSION = $ELK_VERSION.split(".")[2]
@@ -67,29 +69,57 @@ function determine_elk_version() {
     }
 }
 
-function install_7zip_pwsh() {
-    Install-Package -Scope CurrentUser -Force 7Zip4PowerShell > $null
+function encrypt_es_passwords() {
+    # Extract Elasticsearch pscipher and psvault to use for encryption
+    7z x $ELK_INSTALL\archives\pt-elasticsearch-$ELK_VERSION.tgz -o"$env:TEMP\es\" 2>&1 | out-null
+    7z e $env:TEMP\es\pt-elasticsearch-$ELK_VERSION.tgz -o"$env:TEMP\es\" psvault -r -aoa 2>&1 | out-null
+    7z e $env:TEMP\es\pt-elasticsearch-$ELK_VERSION.tgz -o"$env:TEMP\es\" pscipher.jar -r -aoa 2>&1 | out-null
+
+    # Encrypt passwords for Elasticsearch
+    $PSCIPHER_PATH="${env:TEMP}\es"
+    $PSCIPHER="${PSCIPHER_PATH}\pscipher.jar"
+    $JAR_LIB="com.peoplesoft.pt.elasticsearch.pscipher.PSESEncrypt"
+    cmd /c "java -Dpscipher.path=${PSCIPHER_PATH} -cp ${PSCIPHER} ${JAR_LIB} esadmin ${ESADMIN_PWD} people ${PEOPLE_PWD} es_password.txt"
+    
+    $esadmin_pass_es = $(gc es_password.txt | select-string esadmin | % {$_.line.split(":")[1]})
+    $people_pass_es = $(gc es_password.txt | select-string people | % {$_.line.split(":")[1]})
+
+    # Cleanup temp files
+    Remove-Item $env:TEMP\es -recurse
+    Remove-Item es_password.txt
 }
 
-function encrypt_es_passwords() {
-    Expand-7Zip $ELK_INSTALL\archives\pt-elasticsearch-$ELK_VERSION $env:TEMP\es
+function encrypt_ls_passwords() {
+    # Extract Logstash pscipher and psvault to use for encryption
+    7z x $ELK_INSTALL\archives\pt-logstash-$ELK_VERSION.tgz -o"$env:TEMP\ls\" 2>&1 | out-null
+    7z x $env:TEMP\ls\pt-logstash-$ELK_VERSION.tgz -o"$env:TEMP\ls\" 2>&1 | out-null
+    
+    # Encrypt Passwords for Logstash
+    $LOGSTASH_HOME="$env:TEMP\ls"
+    $esadmin_pass_ls = (cmd /c  ${LOGSTASH_HOME}\pt\bin\pslscipher.bat ${ESADMIN_PWD}).split(" ")[2]
+    $people_pass_ls = (cmd /c  ${LOGSTASH_HOME}\pt\bin\pslscipher.bat ${PEOPLE_PWD}).split(" ")[2]
+    $ib_user = $db_user
+    $ib_pass = (cmd /c  ${LOGSTASH_HOME}\pt\bin\pslscipher.bat ${PEOPLE_PWD}).split(" ")[2]
+
+    # Cleanup temp files
+    Remove-Item $env:TEMP\ls -recurse
 }
 
 function generate_response_file() {
-    $file = New-Item -type file "${ELK_INSTALL}}/setup/silentinstall.config" -force
+    $file = New-Item -type file "${ELK_INSTALL}/setup/silentinstall.config" -force
 
     $hostname = $(facter hostname)
     $domain = $(facter domain)
+    $piahttpport = $(hiera pia_http_port -c $PUPPET_HOME\hiera.yaml)
+    $db_user = $(hiera db_user -c $PUPPET_HOME\hiera.yaml)
+    $db_user_pwd = $(hiera db_user_pwd -c $PUPPET_HOME\hiera.yaml)
     
-    . install_7zip_pwsh
     . encrypt_es_passwords
     . encrypt_ls_passwords
-    . encrypt_kb_passwords
 
     switch ($APP) {
-        case "FSCM92" {
+        "FSCM92" {
             $nodename = "PSFT_EP"
-            $ib_user  = "VP1"
         }
     }
 
@@ -116,11 +146,7 @@ JSON_files?[Y/N] = y
 Install kibana?[Y/N]= y
 kibana.host= ${hostname}.${domain}
 Use same ES?[Y/N]= y
-ES.host[http(s)://hostname]= http://${hostname}.${domain}
-ES.port= 9200
-ES.password= ${esadmin_pass_kb}
 Upgrade elasticsearch?[Y/N]= n
-
 "@ 
 
     if ($DEBUG -eq "true") {
@@ -129,77 +155,77 @@ Upgrade elasticsearch?[Y/N]= n
     }
     $template | out-file $file -Encoding ascii
 }
-function execute_psft_dpk_setup() {
+# function execute_psft_dpk_setup() {
 
-  # $begin=$(get-date)
-  Write-Output "Executing DPK setup script"
-  Write-Output "DPK INSTALL: ${ELK_INSTALL}}"
+#   # $begin=$(get-date)
+#   Write-Output "Executing DPK setup script"
+#   Write-Output "DPK INSTALL: ${ELK_INSTALL}}"
 
-  sELK ($ELK_MINOR_VERSION) {
-    "58" {
-        Write-Output "Running PeopleTools 8.58 Bootstrap Script"
-        if ($DEBUG -eq "true") {
-            . "${ELK_INSTALL}}/setup/psft-dpk-setup.bat" `
-            --silent `
-            --dpk_src_dir "${ELK_INSTALL}}" `
-            --response_file "${ELK_INSTALL}}/response.cfg" `
-            --no_puppet_run
-        } else {
-            . "${ELK_INSTALL}}/setup/psft-dpk-setup.bat" `
-            --dpk_src_dir ${ELK_INSTALL}} `
-            --silent `
-            --response_file "${ELK_INSTALL}}/response.cfg" `
-            --no_puppet_run 2>&1 | out-null
-        }
-    }
-    "57" {
-        Write-Output "Running PeopleTools 8.57 Bootstrap Script"
-        if ($DEBUG -eq "true") {
-            . "${ELK_INSTALL}}/setup/psft-dpk-setup.bat" `
-            --silent `
-            --dpk_src_dir "${ELK_INSTALL}}" `
-            --response_file "${ELK_INSTALL}}/response.cfg" `
-            --no_puppet_run
-        } else {
-            . "${ELK_INSTALL}}/setup/psft-dpk-setup.bat" `
-            --dpk_src_dir ${ELK_INSTALL}} `
-            --silent `
-            --response_file "${ELK_INSTALL}}/response.cfg" `
-            --no_puppet_run 2>&1 | out-null
-        }
-    } 
-    "56" {
-        Write-Output "Running PeopleTools 8.56 Bootstrap Script"
-        if ($DEBUG -eq "true") {
-            . "${ELK_INSTALL}}/setup/psft-dpk-setup.bat" `
-            --silent `
-            --dpk_src_dir "${ELK_INSTALL}}" `
-            --response_file "${ELK_INSTALL}}/response.cfg" `
-            --no_puppet_run
-        } else {
-            . "${ELK_INSTALL}}/setup/psft-dpk-setup.bat" `
-            --dpk_src_dir ${ELK_INSTALL}} `
-            --silent `
-            --response_file "${ELK_INSTALL}}/response.cfg" `
-            --no_puppet_run 2>&1 | out-null
-        }
-    } 
-    "55" {
-        if ($DEBUG -eq "true") {
-            . "${ELK_INSTALL}}/setup/psft-dpk-setup.ps1" `
-            -dpk_src_dir=$(resolve-path${ELK_INSTALL}).path `
-            -silent `
-            -no_env_setup
-        } else {
-            . "${ELK_INSTALL}}/setup/psft-dpk-setup.ps1" `
-            -dpk_src_dir=$(resolve-path${ELK_INSTALL}).path `
-            -silent `
-            -no_env_setup 2>&1 | out-null
-        }
-    }
-  } # end switch
-}
+#   sELK ($ELK_MINOR_VERSION) {
+#     "58" {
+#         Write-Output "Running PeopleTools 8.58 Bootstrap Script"
+#         if ($DEBUG -eq "true") {
+#             . "${ELK_INSTALL}}/setup/psft-dpk-setup.bat" `
+#             --silent `
+#             --dpk_src_dir "${ELK_INSTALL}}" `
+#             --response_file "${ELK_INSTALL}}/response.cfg" `
+#             --no_puppet_run
+#         } else {
+#             . "${ELK_INSTALL}}/setup/psft-dpk-setup.bat" `
+#             --dpk_src_dir ${ELK_INSTALL}} `
+#             --silent `
+#             --response_file "${ELK_INSTALL}}/response.cfg" `
+#             --no_puppet_run 2>&1 | out-null
+#         }
+#     }
+#     "57" {
+#         Write-Output "Running PeopleTools 8.57 Bootstrap Script"
+#         if ($DEBUG -eq "true") {
+#             . "${ELK_INSTALL}}/setup/psft-dpk-setup.bat" `
+#             --silent `
+#             --dpk_src_dir "${ELK_INSTALL}}" `
+#             --response_file "${ELK_INSTALL}}/response.cfg" `
+#             --no_puppet_run
+#         } else {
+#             . "${ELK_INSTALL}}/setup/psft-dpk-setup.bat" `
+#             --dpk_src_dir ${ELK_INSTALL}} `
+#             --silent `
+#             --response_file "${ELK_INSTALL}}/response.cfg" `
+#             --no_puppet_run 2>&1 | out-null
+#         }
+#     } 
+#     "56" {
+#         Write-Output "Running PeopleTools 8.56 Bootstrap Script"
+#         if ($DEBUG -eq "true") {
+#             . "${ELK_INSTALL}}/setup/psft-dpk-setup.bat" `
+#             --silent `
+#             --dpk_src_dir "${ELK_INSTALL}}" `
+#             --response_file "${ELK_INSTALL}}/response.cfg" `
+#             --no_puppet_run
+#         } else {
+#             . "${ELK_INSTALL}}/setup/psft-dpk-setup.bat" `
+#             --dpk_src_dir ${ELK_INSTALL}} `
+#             --silent `
+#             --response_file "${ELK_INSTALL}}/response.cfg" `
+#             --no_puppet_run 2>&1 | out-null
+#         }
+#     } 
+#     "55" {
+#         if ($DEBUG -eq "true") {
+#             . "${ELK_INSTALL}}/setup/psft-dpk-setup.ps1" `
+#             -dpk_src_dir=$(resolve-path${ELK_INSTALL}).path `
+#             -silent `
+#             -no_env_setup
+#         } else {
+#             . "${ELK_INSTALL}}/setup/psft-dpk-setup.ps1" `
+#             -dpk_src_dir=$(resolve-path${ELK_INSTALL}).path `
+#             -silent `
+#             -no_env_setup 2>&1 | out-null
+#         }
+#     }
+#   } # end switch
+# }
 
 . determine_elk_version
 . generate_response_file
-. execute_psft_dpk_setup
+#. execute_psft_dpk_setup
--- powershell/provision-utilities.ps1
+++ powershell/provision-utilities.ps1
@@ -72,9 +72,16 @@ function install_code_management() {
 	choco install git -y
 }
 
+function install_command_line_utils() {
+  Write-Output "[${env:COMPUTERNAME}] Installing Command Line Utilities"
+  choco install grep -y
+  choco install 7zip - y
+}
+
 #-----------------------------------------------------------[Execution]-----------------------------------------------------------
 
 
 . install_browsers
 . install_code_management
 . install_psadmin_plus
+. install_command_line_utils
