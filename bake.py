5da2efa79c0bda0561d2a4d73ba8822b37eedaba
Kyle Benson <kyle@psadmin.io>
2020-08-19 18:01:57 +0000

    logging and basic structure working

--- bake.py
+++ bake.py
@@ -2,18 +2,34 @@ import os
 import logging
 
 from shutil import copyfile
+from pathlib import Path
 
-MOS_USERNAME = os.environ["MOS_USERNAME"]
-MOS_PASSWORD = os.environ["MOS_PASSWORD"]
-PI_PATCH_ID  = os.environ["PI_PATCH_ID"]
-ELK_PATCH_ID = os.environ["ELK_PATCH_ID"]
-
-logfile  = "c:\temp\psft-pi-baker.log" # TODO
-loglevel = logging.DEBUG 
-logging.basicConfig(filename=logfile, level=loglevel)
+MOS_USERNAME = os.getenv("MOS_USERNAME")
+MOS_PASSWORD = os.getenv("MOS_PASSWORD")
+PI_PATCH_ID  = os.getenv("PI_PATCH_ID")
+ELK_PATCH_ID = os.getenv("ELK_PATCH_ID")
 
 files_dir = os.path.dirname(os.path.realpath(__file__)) + "/files"
 userdata_dir = os.path.dirname(os.path.realpath(__file__)) + "/userdata"
+
+# Logging
+logpath = "."
+logfile  = "psft-pi-baker.log" # TODO
+loglevel = logging.DEBUG 
+rootLogger = logging.getLogger()
+rootLogger.setLevel(loglevel)
+
+fileHandler = logging.FileHandler("{0}/{1}".format(logpath, logfile))
+fileFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+fileHandler.setFormatter(fileFormatter)
+fileHandler.setLevel(loglevel)
+rootLogger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleFormatter = logging.Formatter("[%(levelname)-5.5s]  %(message)s")
+consoleHandler.setFormatter(consoleFormatter)
+consoleHandler.setLevel(loglevel)
+rootLogger.addHandler(consoleHandler)
 
 # CRITICAL 50
 # ERROR    40
@@ -21,65 +37,67 @@ userdata_dir = os.path.dirname(os.path.realpath(__file__)) + "/userdata"
 # INFO     20
 # DEBUG    10
 
-#     Push-Location $PSScriptRoot
-
-#     Function log($msg) {
-#         $stamp = [DateTime]::Now.ToString("yyyyMMdd-HHmmss")
-#         Add-Content $log "[$stamp] $msg"
-#     }
-#     Function info($msg) {
-#         log("INFO: $msg")
-#     }
-#     Function debug($msg) {
-#         log("DEBUG: $msg")
-#     }
-#     Function error($msg) {
-#         log("ERROR: $msg")
-#     }    
-
-
 def banner():
-    puts "banner goes here TODO"
+    logging.debug("Banner start")
+
+    print("banner goes here TODO")
+
+    logging.debug("Banner stop")
 
 def setup_filesystem():
     
+    logging.debug("Create base directory, if needed.")
     base_dir = "/u01/app/oracle/product"
-    dpk_dir  = base_dir + "hostname/dpk" # TODO - actually use hostname here, like CM does?
-    home_dir  = base_dir + "hostname/home" # TODO - actually use hostname here, like CM does?
-    cfg_dir  = base_dir + "hostname/ps_cfg_home" # TODO - actually use hostname here, like CM does?
+    logging.debug("Create dpk directory, if needed.")
+    dpk_dir  = base_dir + "/hostname/dpk" # TODO - actually use hostname here, like CM does?
+    logging.debug("Create home directory, if needed.")
+    home_dir  = base_dir + "/hostname/home" # TODO - actually use hostname here, like CM does?
+    logging.debug("Create PS_CFG_HOME directory, if needed.")
+    cfg_dir  = base_dir + "/hostname/ps_cfg_home" # TODO - actually use hostname here, like CM does?
 
     try:
-        os.makedirs(base_dir) # TODO - mode?
-        os.makedirs(dpk_dir) # TODO - mode?
-        os.makedirs(home_dir) # TODO - mode?
-        os.makedirs(cfg_dir) # TODO - mode?
+        Path(base_dir).mkdir(parents=True, exist_ok=True) # TODO - mode?
+        Path(dpk_dir).mkdir(parents=True, exist_ok=True) # TODO - mode?
+        Path(home_dir).mkdir(parents=True, exist_ok=True) # TODO - mode?
+        Path(cfg_dir).mkdir(parents=True, exist_ok=True) # TODO - mode?
 
         # copyfile(files_dir + "/vagabond.json",userdata_dir) TODO
         # psft_cust.yaml? TODO
-    
-    except:
-        puts "Error - TODO"
+   
+    except FileExistsError as e:
+        logging.debug(e)
+        pass
+
+    except Exception as e:
+        logging.error("There was an issue setting up the file system.")
+        logging.error(e)
 
 def download():
     # & ./powershell/provision-download.ps1 -MOS_USERNAME "$MOS_USERNAME" -MOS_PASSWORD "$MOS_PASSWORD" -PATCH_ID "$PI_PATCH_ID" -DPK_INSTALL "c:/psft/dpk/downloads/$PI_PATCH_ID" >> $log
-    
+    logging.info("Downloading DPK zip files. - TODO")
+
 def bootstrap():
     # & ./powershell/provision-bootstrap-ps.ps1 -PATCH_ID "$PI_PATCH_ID" -DPK_INSTALL "c:/psft/dpk/downloads/$PI_PATCH_ID" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet" >> $log
+    logging.info("TODO")
 
 def yaml():
     # & ./powershell/provision-yaml.ps1 -DPK_INSTALL "c:/psft/dpk/downloads/$PI_PATCH_ID" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet" >> $log
+    logging.info("TODO")
 
 def puppet_apply():
     # & ./powershell/provision-puppet-apply.ps1 -DPK_INSTALL "c:/psft/dpk/downloads/$PI_PATCH_ID" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet" >> $log
+    logging.info("TODO")
     
 def util():
     # & ./powershell/provision-utilities.ps1 >> $log
+    logging.info("TODO")
 
 def done():
     # done banner?
+    logging.info("TODO")
 
 def main():
-    banner
+    banner()
     setup_filesystem()
     download()
     bootstrap()
