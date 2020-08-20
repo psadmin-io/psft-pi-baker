274405f97fddffe230b7ea534b931268993f4cde
kbens <kyle@psadmin.io>
2020-08-20 03:32:15 +0000

    puppet apply working

--- bake.py
+++ bake.py
@@ -12,6 +12,7 @@ ELK_PATCH_ID = os.getenv("ELK_PATCH_ID")
 
 files_dir = os.path.dirname(os.path.realpath(__file__)) + "/files"
 userdata_dir = os.path.dirname(os.path.realpath(__file__)) + "/userdata"
+custom_dir = userdata_dir + "/custom"
 
 # Logging
 logpath = userdata_dir
@@ -46,20 +47,18 @@ def banner():
     logging.debug("Banner stop")
 
 def setup_filesystem():
-    
-    logging.debug("Create base directory, if needed.")
-    base_dir = "/u01/app/oracle/product"
-    logging.debug("Create dpk directory, if needed.")
-    dpk_dir  = base_dir + "/hostname/dpk" # TODO - actually use hostname here, like CM does?
-    logging.debug("Create home directory, if needed.")
-    home_dir  = base_dir + "/hostname/home" # TODO - actually use hostname here, like CM does?
-    logging.debug("Create PS_CFG_HOME directory, if needed.")
-    cfg_dir  = base_dir + "/hostname/ps_cfg_home" # TODO - actually use hostname here, like CM does?
+
+# TODO
+#sudo yum -y install oracle-database-preinstall-19c glibc-devel
 
     try:
-        Path(base_dir).mkdir(parents=True, exist_ok=True) # TODO - mode?
+        logging.debug("Create base directory, if needed.")
+        Path(psft_base_dir).mkdir(parents=True, exist_ok=True) # TODO - mode?
+        logging.debug("Create dpk directory, if needed.")
         Path(dpk_dir).mkdir(parents=True, exist_ok=True) # TODO - mode?
+        logging.debug("Create home directory, if needed.")
         Path(home_dir).mkdir(parents=True, exist_ok=True) # TODO - mode?
+        logging.debug("Create PS_CFG_HOME directory, if needed.")
         Path(cfg_dir).mkdir(parents=True, exist_ok=True) # TODO - mode?
 
         # copyfile(files_dir + "/vagabond.json",userdata_dir) TODO
@@ -118,37 +117,91 @@ def bootstrap(dpk_files_dir, psft_base_dir, puppet_home):
         logging.warning("Windows is not supported at this time.")
     else:
         setup_script = dpk_files_dir + "/setup/psft-dpk-setup.sh"
-        subprocess.run(["sh", setup_script, "--silent", "--dpk_src_dir " + dpk_files_dir, "--response_file " + dpk_files_dir + "/response.cfg", "--no_puppet_run"])
+        dpk_logfile = open(userdata_dir + "/psft-dpk-setup.log","w")
+        try:
+            subprocess.run(["sh", setup_script, "--silent", "--dpk_src_dir " + dpk_files_dir, "--response_file " + dpk_files_dir + "/response.cfg", "--no_puppet_run"], stdout=dpk_logfile, stderr=dpk_logfile, check=True)
+        except:
+            logging.error("DPK bootstrap script failed.")
 
-def yaml():
-    # & ./powershell/provision-yaml.ps1 -DPK_INSTALL "c:/psft/dpk/downloads/$PI_PATCH_ID" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet" >> $log
-    logging.info("TODO")
+def yaml(puppet_home):
+    logging.info("Copying psft_customizations.yaml")
+    # TODO assuming data directory is there, add error check
+    copyfile(custom_dir + "/psft_customizations.yaml", puppet_home + "/production/data/psft_customizations.yaml")
 
-def puppet_apply():
-    # & ./powershell/provision-puppet-apply.ps1 -DPK_INSTALL "c:/psft/dpk/downloads/$PI_PATCH_ID" -PSFT_BASE_DIR "c:/psft" -PUPPET_HOME "c:/psft/dpk/puppet" >> $log
+def puppet_apply(puppet_home):
     logging.info("TODO")
+  
+    # execute_puppet_apply
+    # TODO - keep for future windows usage?
+    # Reset Environment and PATH to include bin\puppet
+    # $env:PATH = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+        
+    try:
+        dpk_logfile = open(userdata_dir + "/psft-dpk-apply.log","w")
+        subprocess.run(["sudo","/opt/puppetlabs/bin/puppet", "apply", puppet_home + "/production/manifests/site.pp", "--confdir=" + puppet_home, "--trace", "--debug"], check=True, stdout=dpk_logfile, stderr=dpk_logfile)
+        #subprocess.run(["sudo","/opt/puppetlabs/bin/puppet", "--version"], check=True, stdout=dpk_logfile, stderr=dpk_logfile)
+    except:
+        logging.error("Puppet apply failed.")
     
 def util():
     # & ./powershell/provision-utilities.ps1 >> $log
-    logging.info("TODO")
+    logging.info("TODO-util")
+
+    # # Fix Ruby Gems CA
+  # https://gist.github.com/iversond/772e73257c4ca59a9e6137baa7288788
+  # $CACertFile = Join-Path -Path $ENV:AppData -ChildPath 'RubyCACert.pem'
+
+  # If (-Not (Test-Path -Path $CACertFile)) {  
+    #"Downloading CA Cert bundle.."
+  #   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+  #   Invoke-WebRequest -Uri 'https://curl.haxx.se/ca/cacert.pem' -UseBasicParsing -OutFile $CACertFile | Out-Null
+  # }
+
+  # Update PATH
+  # Write-Output "[${env:COMPUTERNAME}] Adding gem and git to PATH"
+  # $env:PATH+=";C:\Program Files\Puppet Labs\Puppet\sys\ruby\bin;C:\Program Files\Git\bin"
+  # [System.Environment]::SetEnvironmentVariable('PATH',$env:PATH, [System.EnvironmentVariableTarget]::Machine)
+
+  # "Setting CA Certificate store set to $CACertFile.."
+ #  $ENV:SSL_CERT_FILE = $CACertFile
+ #  [System.Environment]::SetEnvironmentVariable('SSL_CERT_FILE',$CACertFile, [System.EnvironmentVariableTarget]::Machine)
+
+#   gem install psadmin_plus
+# }
+
+# function install_browsers() {
+#   Write-Output "[${env:COMPUTERNAME}] Installing Browsers"
+#   choco install googlechrome -y
+# 	choco install firefox -y
+# }
+
+# function install_code_management() {
+#   Write-Output "[${env:COMPUTERNAME}] Installing Code Management Software"
+# }  choco install VSCode -y
+# 	choco install git -y
+
+
 
 def done():
-    # done banner?
-    logging.info("TODO")
+    logging.info("Done.")
 
 def main():
+    # TODO
+    psft_base_dir = "/u01/app/oracle/product"
+    dpk_files_dir = userdata_dir + "/11"
+    dpk_dir       = psft_base_dir + "/dpk" # TODO - actually use hostname here, like CM does?
+    home_dir      = psft_base_dir + "/hostname/home" # TODO - actually use hostname here, like CM does?
+    cfg_dir       = psft_base_dir + "/hostname/ps_cfg_home" # TODO - actually use hostname here, like CM does?
+    puppet_home   = dpk_dir + "/puppet" # TODO - install dpk/ in 
+    
     banner()
     setup_filesystem()
     # download()
-    # TODO
-    dpk_files_dir = userdata_dir + "/11"
-    psft_base_dir = "/u01/app/oracle/product"
-    puppet_home   = psft_base_dir + "/hostname/dpk"
-    bootstrap(dpk_files_dir, psft_base_dir, puppet_home)
-    # yaml()
-    # puppet_apply()
+    #bootstrap(dpk_files_dir, psft_base_dir, puppet_home)
+    yaml(puppet_home)
+    puppet_apply(puppet_home)
     # util()
-    # done()
+    done()
 
 if __name__ == "__main__":
     main()
